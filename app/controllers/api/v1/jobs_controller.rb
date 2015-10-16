90a530c50f0f9ed68b253f9c1ca7bc2f51b614f9
Kyle Swartz <kyle.swartz.3@gmail.com>
2015-10-16 09:41:42 -0400

    testing json response format.

--- app/controllers/api/v1/jobs_controller.rb
+++ app/controllers/api/v1/jobs_controller.rb
@@ -2,12 +2,15 @@ class Api::V1::JobsController < ApplicationController
 
   def show
     job = Job.find(params[:id])
-    job_json = job.as_json
-    job_json['interpreter'] = job.interpreter
-    job_json['requester'] = job.requester
+    # job_json = job.as_json
+    # job_json['interpreter'] = job.interpreter
+    # job_json['requester'] = job.requester
     render json: {
       success: true,
-      job: job_json
+      job: job.as_json(include: {
+        interpreter: job.interpreter,
+        requester: job.requester
+      })
       },
       status: :ok
   end
